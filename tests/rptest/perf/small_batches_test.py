57bd38296943fd09d22e2d273c3aed954f653f02
Bharath Vissapragada <bharathv@redpanda.com>
2024-01-31 15:37:48 -0800

    small batches test changes to run locally

--- tests/rptest/perf/small_batches_test.py
+++ tests/rptest/perf/small_batches_test.py
@@ -12,18 +12,25 @@ class SmallBatchesTest(RedpandaTest):
         self._ctx = ctx
         super(SmallBatchesTest,
               self).__init__(test_context=ctx,
-                             extra_rp_conf={"aggregate_metrics": True})
+                             extra_rp_conf={
+                                 "aggregate_metrics": True,
+                                 "append_chunk_size": 1048756,
+                                 "segment_appender_flush_timeout_ms": 5000,
+                                 "raft_replica_max_pending_flush_bytes":
+                                 2097152,
+                                 "cache_writes": True
+                             })
 
     @cluster(num_nodes=6)
     def omb_test(self):
         workload = {
             "name": "SmallBatchesWorkload",
             "topics": 1,
-            "partitions_per_topic": 20000,
+            "partitions_per_topic": 1,
             "subscriptions_per_topic": 1,
-            "consumer_per_subscription": 200,
-            "producers_per_topic": 200,
-            "producer_rate": 150_000,
+            "consumer_per_subscription": 2,
+            "producers_per_topic": 5,
+            "producer_rate": 150_00,
             "message_size": 1024,
             "payload_file": "payload/payload-1Kb.data",
             "consumer_backlog_size_GB": 0,
